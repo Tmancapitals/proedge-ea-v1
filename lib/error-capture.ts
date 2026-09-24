f2d2228a47ff288c155e4cb6a358d2d46d480695
Tmancapitals <seakethabang6@gmail.com>
2026-09-24 09:19:24 +0200

    Add newline at end of error-capture.ts
    Fix missing newline at end of file.

---
+++ lib/error-capture.ts
@@ -0,0 +1,16 @@
+
+let lastCapturedError: unknown;
+
+export function captureError(error: unknown) {
+  lastCapturedError = error;
+}
+
+export function consumeLastCapturedError(): Error | undefined {
+  const error = lastCapturedError;
+  lastCapturedError = undefined;
+
+  if (error instanceof Error) return error;
+  if (error == null) return undefined;
+
+  return new Error(String(error));
+}
